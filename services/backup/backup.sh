d89b881e1d51a9e5640051fe04d6619d5254cd44
Philipp Kraft <philipp.kraft@envr.jlug.de>
2022-02-17 21:36:15 +0100

    Created a backup service. Fixed #1. Needs testing

---
+++ services/backup/backup.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+
+# Change this to some magic with $0 getting the directory for the script
+TODAY=$(date -I)
+PERIOD=${PERIOD:-$TODAY}
+FILE=/var/backup/$PERIOD.sql.$BACKUP_ZIP
+pg_dump -a --dbname=$DB_URL | $BACKUP_ZIP >$FILE
+echo $(date '+%Y-%m-%d %H:%M') $FILE >>/var/backup/log
