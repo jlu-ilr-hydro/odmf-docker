82c4e27a4c2c1719042a678987775952c0272c2c
Philipp Kraft <philipp.kraft@envr.jlug.de>
2022-02-21 16:48:02 +0100

    Debugging backup

--- services/odmf/db-restore.sh
+++ services/odmf/db-restore.sh
@@ -4,6 +4,6 @@ TABLES=$(odmf tables)
 ODMF_NAME=$(basename $DB_URL)
 for T in $TABLES
 do
-  psql -d $DB_URL -c "DELETE FROM $T"
+  psql -d $DB_URL -c "DELETE FROM $T;"
   gunzip -c $T.$ODMF_NAME.*.sql.gz | psql -d $DB_URL
 done
