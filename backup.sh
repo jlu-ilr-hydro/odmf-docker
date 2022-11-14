5c162f49dbcf39cd0966f14a828c6f462e377e8c
philipp <Philipp Kraft>
2022-11-14 18:20:01 +0100

    backup.sh

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pushd "$(dirname ${BASH_SOURCE:0})"
+trap popd EXIT
+cp /srv/odmf/template/services/pg-backup/db-backup.sh ./backup_pg
+# Use echo to strip whitespace
+[ ! -f ./backup_pg/tables ] && echo $(docker-compose exec web odmf db-tables) | dos2unix > ./backup_pg/tables
+/usr/bin/docker-compose exec -T db bash /var/backup/db-backup.sh $1
